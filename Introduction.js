8e6a1d9fba37751f8f06d177180e8ba7a844efd2
Rutuja949 <80196441+Rutuja949@users.noreply.github.com>
2023-02-02 16:26:21 +0530

    Add files via upload

---
+++ Introduction.js
@@ -0,0 +1,49 @@
+//Print Masai School
+
+let a = "Masai School";
+let b = "A Transformation in Education";
+console.log(a);
+console.log(b);
+
+//Assigning a new value to variable
+
+let name = "Rutuja";
+console.log(name);
+
+name = "Prabhakar";
+console.log(name);
+
+name = "Yogita";
+console.log(name);
+
+//Print name and age
+
+let name = "Rutuja";
+let age = 22;
+console.log(name, age);
+console.log(typeof(name));
+console.log(typeof(age));
+
+//Print a report card
+
+let a = "Report Card";
+let name = "Name:          Jin";
+let age = "Age :          14";
+let school = "SchoolName :   Holy cross English school";
+let grade = "Grade:         A";
+let section = "Section:       C";
+let rollno = "Rollno:        324";
+let marks1 = "Marks covered in English: 150/180";
+let marks2 = "Marks covered in Maths:   165/180";
+let marks3 = "Marks covered in Science: 172/180";
+
+console.log(a);
+console.log(name);
+console.log(age);
+console.log(school);
+console.log(grade);
+console.log(section);
+console.log(rollno);
+console.log(marks1);
+console.log(marks2);
+console.log(marks3);
